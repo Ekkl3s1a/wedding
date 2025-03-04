6bb5130593fc399d8ed9a816b8735f94a657f10a
Brnuo <F13ND5@protonmail.com>
2025-03-04 16:15:44 +0000

    Add titlecase && reorganize folders

--- wedding-site/src/app/app.component.ts
+++ wedding-site/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FaqComponent } from './components/faq/faq.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { MapComponent } from './components/map/map.component';
 import { ScheduleComponent } from './components/schedule/schedule.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
